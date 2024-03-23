2fca3a61de39a56e131110d2995dbbaa5afea799
Rvssm-Sandeep <manisandeep2004@gmail.com>
2024-03-23 10:07:05 +0530

    first commit

--- counter/views.py
+++ counter/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-
 # Create your views here.
 def home(request):
     import json
@@ -9,7 +8,7 @@ def home(request):
         query = request.POST['query']
         api_url = 'https://api.api-ninjas.com/v1/nutrition?query='
         api_request = requests.get(
-            api_url + query, headers={'X-Api-Key': 'YOUR-API-KEY'})
+            api_url + query, headers={'X-Api-Key': '+B0uAbRGGi5/u47Mu5BcLA==lOsgbBpZ9UfuwMY9'})
         try:
             api = json.loads(api_request.content)
             print(api_request.content)
